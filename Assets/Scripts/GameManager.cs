5ef53587b3eacf0e6b8857d771414b5f88db852b
ss-zun <zxq0158@gmail.com>
2024-09-05 02:04:06 +0900

    [feat] 적 공통 프리팹 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public Player Player;
     public ObjectPool ObjectPool;
 
     private void Awake()
